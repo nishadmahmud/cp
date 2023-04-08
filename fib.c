99863f2acc25524c05fb1f187d5e96562770b8ec
Nishad <86399706+nishadmahmud@users.noreply.github.com>
2023-04-08 07:12:39 +0000

    fib

--- fib.c
+++ fib.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
-
-int arr[1000000007];
+#define ll long long
 int main()
 {
-    // print fibonacci series of first n numbers
-    int n, i;
+    ll n, i;
     printf("Enter the number of terms: ");
-    scanf("%d", &n);
-    arr[0] = 0;
-    arr[1] = 1;
+    scanf("%lld", &n);
+    ll arr[n];
+    arr[0] = 0, arr[1] = 1;
     for (i = 2; i < n; i++)
-    {
         arr[i] = arr[i - 1] + arr[i - 2];
-    }
+
     printf("Fibonacci Series: ");
     for (i = 0; i < n; i++)
-    {
-        printf("%d ", arr[i]);
-    }
+        printf("%lld ", arr[i]);
+
+    printf("\n");
     return 0;
 }

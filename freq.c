1100c7f6fa3c51f072c835a9f6a705b574163ed4
Nishad <86399706+nishadmahmud@users.noreply.github.com>
2023-04-08 08:14:12 +0000

    change to isk

--- freq.c
+++ freq.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
     int n, count, visit = -1;
     scanf("%d", &n);
     int arr[n], freq[n];
     for (int i = 0; i < n; i++)
         scanf("%d", &arr[i]);
 
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
         count = 1;
-        for (int j = i + 1; j < n; j++){
-            if (arr[i] == arr[j]){
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] == arr[j])
+            {
                 count++;
                 freq[j] = visit;
             }
@@ -19,7 +23,8 @@ int main(){
             freq[i] = count;
     }
 
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
         if (freq[i] != visit)
             printf("%d occurs %d times\n", arr[i], freq[i]);
     }
--- isk.c
+++ isk.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
 int arr[1000000007];
-int main(){
+int main()
+{
     int n, x, find;
     scanf("%d", &n);
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
         scanf("%d", &x);
-        arr[x] = i;
+        arr[x] = i + 1;
     }
     scanf("%d", &find);
     if (arr[find])
-        printf("Found at index %d\n", arr[find]);
+        printf("Found at index %d\n", arr[find] - 1);
     else
         printf("Not found\n");
     return 0;
